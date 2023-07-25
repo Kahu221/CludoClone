ec8beadc2dfacc610df2f7c0cc7c1ab80d68d6fe
Jackson Rakena <rakenajack@cross-bar.ecs.vuw.ac.nz>
2023-07-26 10:44:03 +1200

    Initial

---
+++ src/hobby_detectives/Main.java
@@ -0,0 +1,7 @@
+package hobby_detectives;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rakenajack
+ *
+ */
+module hobby_detectives {
+}
