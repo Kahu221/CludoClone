3e503592258b4c7136e1ec60f80e1764d14fb903
jacksonrakena <jackson@rakena.co.nz>
2023-08-23 13:19:14 +1200

    Add test framework

---
+++ src/hobby_detectives/test/GuiTests.java
@@ -0,0 +1,21 @@
+package hobby_detectives.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class GuiTests {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,7 @@
  */
 module hobby_detectives {
     requires java.desktop;
+    requires junit;
+
+    exports hobby_detectives.test to junit;
 }
